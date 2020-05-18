5c5b78a4c17dcfe082c7333767eb9a4e6f2a49bc
TemplierPaul <templier.paul@gmail.com>
2020-05-18 15:19:58 +0200

    New server to test a decision function as parameter

--- agent_server_NEAT.py
+++ agent_server_NEAT.py
@@ -19,11 +19,44 @@ parser.add_option("--portb", type="string", dest="breezyPort", default="8085")
 """
 Handles HTTP requests from the dota server.
 """
+
+
 class ServerHandler(BaseHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super(ServerHandler, self).__init__(*args, **kwargs)
+        self._decision_func = None
+        self._eval_func = None
+
+    """
+    Decision function getter and setter
+    """
+
+    @property
+    def decision_func(self):
+        assert self._decision_func is not None
+        return self._decision_func
+
+    @decision_func.setter
+    def decision_func(self, func):
+        self._decision_func = func
+
+    """
+    Evaluation function setter and getter
+    """
+
+    @property
+    def eval_func(self):
+        assert self._eval_func is not None
+        return self._eval_func
+
+    @eval_func.setter
+    def eval_func(self, func):
+        self._eval_func = func
 
     """
     Helper function to get content passed with http request.
     """
+
     def getContent(self):
         cLen = int(self.headers["Content-Length"])
         return self.rfile.read(cLen)
@@ -31,6 +64,7 @@ class ServerHandler(BaseHTTPRequestHandler):
     """
     Sends a response containing a json object (usually the action).
     """
+
     def postResponse(self, response):
         # response code and info
         self.send_response(200)
@@ -42,44 +76,46 @@ class ServerHandler(BaseHTTPRequestHandler):
     """
     GET used for testing connection.
     """
+
     def do_GET(self):
         print(self.path)
         print("Received GET request.")
-        self.postResponse(json.dumps({"response":"Hello"}))
+        self.postResponse(json.dumps({"response": "Hello"}))
 
     """
     POST used for getting features and returning action.
     """
+
     def do_POST(self):
         print(self.path)
-        
-        if self.path == "/update": 
+
+        if self.path == "/update":
             """
             Update route is called, game finished.
             """
-            
+
             global breezyIp
             global breezyPort
-            
+
             print("Game done.")
             content = self.getContent().decode("utf-8")
             print(content)
             rundata = json.loads(content)
-            
+
             # webhook to start new game in existing set of games
             if "webhook" in rundata:
                 """
                 A webhook was sent to the agent to start a new game in the current
                 set of games.
                 """
-            
+
                 webhookUrl = "http://{}:{}{}".format(
                     breezyIp, breezyPort, rundata["webhook"])
                 print(webhookUrl)
-                
+
                 # call webhook to trigger new game
                 response = requests.get(url=webhookUrl)
-                
+
             # otherwise start new set of games, or end session
             else:
                 """
@@ -89,73 +125,89 @@ class ServerHandler(BaseHTTPRequestHandler):
                 In here would probably be where you put the code to ready a new agent
                 (update NN weights, evolutions, next agent in current gen. etc.).
                 """
-                
-                # build url to dota 2 breezy server
-                startUrl = "http://{}:{}/run/".format(
-                    breezyIp, breezyPort)
-                # create a run config for this agent, to run 5 games
-                startData = {
-                    "agent": "Sample Random Agent",
-                    "size": 5
-                }
-                # response = requests.post(url=startUrl, data=json.dumps(startData))
-                
+
+                """
+                Stop server
+                """
+                global keep_server_up
+                keep_server_up = False
+
             # send whatever to server
             # self.postResponse(json.dumps({"fitness":42}))
-            
-        else: # relay path gives features from current game to agent
+
+        else:  # relay path gives features from current game to agent
             """
             Relay route is called, gives features from the game for the agent.
             """
-            
+
             print("Received features.")
             # get data as json, then save to list
             content = self.getContent().decode("utf-8")
             features = json.loads(content)
             print(features)
-        
+
             """
             Agent code to determine action from features would go here.
             """
-            action = random.randint(0,29) # just random action for this example
-            self.postResponse(json.dumps({"actionCode":action}))
+            action = self.decision_func(features)
+            self.postResponse(json.dumps({"actionCode": action}))
 
-if __name__ == "__main__":
-    """
-    Sets up and starts the Agent server and triggers the start of a run on the 
-    Breezy server.
-    """
 
+def start_server(decision_func, eval_func):
+    """
+        Sets up and starts the Agent server and triggers the start of a run on the
+        Breezy server.
+    """
     # start the Agent server in other thread
     print("Agent Server starting at {}:{}...".format(opts.agentIp, opts.agentPort))
     agentHandler = HTTPServer((opts.agentIp, int(opts.agentPort)), ServerHandler)
+
+    # Set GA functions
+    agentHandler.decision_func = decision_func
+    agentHandler.eval_func = eval_func
+
     thread = threading.Thread(target=agentHandler.serve_forever)
     thread.daemon = True
     thread.start()
     print("Agent server started.")
 
-    # create a run config for this agent, to run 5 games, send to breezy server
+    # create a run config for this agent, to run 1 game, send to breezy server
     startData = {
-        "agent": "Sample Random Agent",
-        "size": 5
+        "agent": "NEAT Agent",
+        "size": 1
     }
     # tell breezy server to start the run
-    # response = requests.post(
-    #     url="http://{}:{}/run/".format(opts.breezyIp, opts.breezyPort),
-    #     data=json.dumps(startData))
-    #
-    # print(response)
-    
+    response = requests.post(
+        url="http://{}:{}/run/".format(opts.breezyIp, opts.breezyPort),
+        data=json.dumps(startData))
+
+    print(response)
+
     """
-    Declare variables global that you want the agent server to have access to.
-    """
+        Declare variables global that you want the agent server to have access to.
+        """
     global breezyIp
     global breezyPort
-    
+
     breezyIp = opts.breezyIp
     breezyPort = opts.breezyPort
 
-    # serve until force stop
-    while True:
+    # serve until told to stop (end of the game)
+    global keep_server_up
+    keep_server_up = True
+
+    while keep_server_up:
         pass
-    
+
+
+if __name__ == "__main__":
+    def decision(arg):
+        print("%d Features" % len(arg))
+        return random.randint(0, 29)
+
+
+    def eval(*args):
+        return 0
+
+
+    start_server(decision_func=decision, eval_func=eval)
